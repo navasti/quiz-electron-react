0dc8fce855f93ea0d1bfb811a89e101537b04ee9
Paweł Szostak <szostakpawel00gmail.com>
2021-11-04 00:04:18 +0100

    Add some questions

---
+++ src/questions.ts
@@ -0,0 +1,58 @@
+export default [
+  {
+    power: 5,
+    question: 'In 1768, Captain James Cook set out to explore which ocean?',
+    correctAnswer: 'A',
+    answers: {
+      A: 'Pacific Ocean',
+      B: 'Atlantic Ocean',
+      C: 'Indian Ocean',
+      D: 'Arctic Ocean'
+    }
+  },
+  {
+    power: 2,
+    question: 'What is actually electricity?',
+    correctAnswer: 'C',
+    answers: {
+      A: 'A flow of water',
+      B: 'A flow of air',
+      C: 'A flow of electrons',
+      D: 'A flow of atoms'
+    }
+  },
+  {
+    power: 1,
+    question: 'Which of the following is not an international organisation?',
+    correctAnswer: 'D',
+    answers: {
+      A: 'FIFA',
+      B: 'NATO',
+      C: 'ASEAN',
+      D: 'FBI'
+    }
+  },
+  {
+    power: 3,
+    question:
+      'Which of the following is a song by the German heavy metal band “Scorpions”?',
+    correctAnswer: 'B',
+    answers: {
+      A: 'Stairway to Heaven',
+      B: 'Wind of Change',
+      C: 'Don’t Stop Me Now',
+      D: 'Hey Jude'
+    }
+  },
+  {
+    power: 4,
+    question: 'What is the speed of sound?',
+    correctAnswer: 'B',
+    answers: {
+      A: '120 km/h',
+      B: '1,200 km/h',
+      C: '400 km/h',
+      D: '700 km/h'
+    }
+  }
+]
