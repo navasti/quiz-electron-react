358fd404f794bd5b2bf4ec97e677bf11c17c5a12
Paweł Szostak <szostakpawel00gmail.com>
2021-11-04 23:12:36 +0100

    Change questions object structure

--- src/questions.ts
+++ src/questions.ts
@@ -1,58 +1,130 @@
 export default [
   {
-    power: 5,
+    difficulty: 5,
+    points: 100000,
     question: 'In 1768, Captain James Cook set out to explore which ocean?',
-    correctAnswer: 'A',
-    answers: {
-      A: 'Pacific Ocean',
-      B: 'Atlantic Ocean',
-      C: 'Indian Ocean',
-      D: 'Arctic Ocean'
-    }
+    correctAnswer: 'Pacific Ocean',
+    answers: ['Atlantic Ocean', 'Indian Ocean', 'Pacific Ocean', 'Arctic Ocean']
   },
   {
-    power: 2,
+    difficulty: 5,
+    points: 100000,
+    question:
+      'The phrase: ”I think, therefore I am” was coined by which philosopher?',
+    correctAnswer: 'Descartes',
+    answers: ['Socrates', 'Plato', 'Aristotle', 'Descartes']
+  },
+  {
+    difficulty: 5,
+    points: 100000,
+    question: 'Which two months are named after Emperors of the Roman Empire?',
+    correctAnswer: 'July and August',
+    answers: [
+      'January and February',
+      'March and April',
+      'May and June',
+      'July and August'
+    ]
+  },
+  {
+    difficulty: 2,
+    points: 5000,
     question: 'What is actually electricity?',
-    correctAnswer: 'C',
-    answers: {
-      A: 'A flow of water',
-      B: 'A flow of air',
-      C: 'A flow of electrons',
-      D: 'A flow of atoms'
-    }
+    correctAnswer: 'A flow of electrons',
+    answers: [
+      'A flow of water',
+      'A flow of air',
+      'A flow of electrons',
+      'A flow of atoms'
+    ]
   },
   {
-    power: 1,
+    difficulty: 2,
+    points: 5000,
+    question: 'Goulash is a type of beef soup in which country?',
+    correctAnswer: 'Hungary',
+    answers: ['Hungary', 'Czech Republic', 'Slovakia', 'Ireland']
+  },
+  {
+    difficulty: 2,
+    points: 5000,
+    question: 'Which is the easiest way to tell the age of many trees?',
+    correctAnswer: 'To count the rings on the trunk',
+    answers: [
+      'To measure the width of the tree',
+      'To count the rings on the trunk',
+      'To count the number of leaves',
+      'To measure the height of the tree'
+    ]
+  },
+  {
+    difficulty: 1,
+    points: 2000,
     question: 'Which of the following is not an international organisation?',
-    correctAnswer: 'D',
-    answers: {
-      A: 'FIFA',
-      B: 'NATO',
-      C: 'ASEAN',
-      D: 'FBI'
-    }
+    correctAnswer: 'FBI',
+    answers: ['FIFA', 'NATO', 'FBI', 'ASEAN']
   },
   {
-    power: 3,
+    difficulty: 1,
+    points: 2000,
+    question: 'Which of the following disorders is the fear of being alone?',
+    correctAnswer: 'Agoraphobia',
+    answers: ['Agoraphobia', 'Aerophobia', 'Acrophobia', 'Arachnophobia']
+  },
+  {
+    difficulty: 3,
+    points: 10000,
     question:
       'Which of the following is a song by the German heavy metal band “Scorpions”?',
-    correctAnswer: 'B',
-    answers: {
-      A: 'Stairway to Heaven',
-      B: 'Wind of Change',
-      C: 'Don’t Stop Me Now',
-      D: 'Hey Jude'
-    }
+    correctAnswer: 'Wind of Change',
+    answers: [
+      'Stairway to Heaven',
+      'Wind of Change',
+      'Don’t Stop Me Now',
+      'Hey Jude'
+    ]
   },
   {
-    power: 4,
+    difficulty: 3,
+    points: 10000,
+    question: 'What do we call a newly hatched butterfly?',
+    correctAnswer: 'A caterpillar',
+    answers: ['A moth', 'A butter', 'A caterpillar', 'A chrysalis']
+  },
+  {
+    difficulty: 3,
+    points: 10000,
+    question:
+      'What was the first country to use tanks in combat during World War I?',
+    correctAnswer: 'Britain',
+    answers: ['France', 'Japan', 'Britain', 'Germany']
+  },
+  {
+    difficulty: 1,
+    points: 50000,
     question: 'What is the speed of sound?',
-    correctAnswer: 'B',
-    answers: {
-      A: '120 km/h',
-      B: '1,200 km/h',
-      C: '400 km/h',
-      D: '700 km/h'
-    }
+    correctAnswer: '1,200 km/h',
+    answers: ['120 km/h', '1,200 km/h', '400 km/h', '700 km/h']
+  },
+  {
+    difficulty: 4,
+    points: 50000,
+    question: 'In total, how many novels were written by the Bronte sisters?',
+    correctAnswer: '7',
+    answers: ['4', '7', '6', '7']
+  },
+  {
+    difficulty: 4,
+    points: 50000,
+    question: 'Which did Viking people use as money?',
+    correctAnswer: 'Jewellery',
+    answers: ['Rune stones', 'Jewellery', 'Seal skins', 'Wool']
+  },
+  {
+    difficulty: 4,
+    points: 50000,
+    question: 'What is the main component of the sun?',
+    correctAnswer: 'Gas',
+    answers: ['Liquid lava', 'Gas', 'Molten iron', 'Rock']
   }
 ]
